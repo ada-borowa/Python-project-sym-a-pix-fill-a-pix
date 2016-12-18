34c2fbefa7e2459d8cd3045e1d6e1d62cb46de07
ada-borowa <ada.borowa@gmail.com>
2016-12-18 22:48:09 +0100

    Reading puzzles from file: reads image, find horizontal and vertical lines, then checks if there is circle in field or on the line. If there is circle classifies color and creates colors list.

--- symapix/imageops/reader.py
+++ symapix/imageops/reader.py
@@ -12,6 +12,7 @@ __email__ = 'ada.borowa@gmail.com'
 
 class SymAPixReader:
     """Reader for sym-a-pix puzzle."""
+
     def __init__(self, filename):
         """ Reads puzzle from picture.
         :param filename: name of file with image of puzzle.
@@ -31,14 +32,53 @@ class SymAPixReader:
         :return: puzzle
         """
         self.rho_horizontal, self.rho_vertical = get_line_positions(self.img_gray)
-        puzzle = Container(len(self.rho_horizontal) - 1)
+        puzzle = Container((len(self.rho_horizontal) - 1, len(self.rho_vertical) - 1))
+        # normal
+        for x in range(len(self.rho_horizontal) - 1):
+            for y in range(len(self.rho_vertical) - 1):
+                puzzle.insert(self.cut_image(x, y, 0), x, y, 0)
+        # shifted in x
+        for x in range(1, len(self.rho_horizontal) - 1):
+            for y in range(len(self.rho_vertical) - 1):
+                puzzle.insert(self.cut_image(x, y, 1), x, y, 1)
+        # shifted in y
+        for x in range(len(self.rho_horizontal) - 1):
+            for y in range(1, len(self.rho_vertical) - 1):
+                puzzle.insert(self.cut_image(x, y, 2), x, y, 2)
+        # shifted in both
+        for x in range(1, len(self.rho_horizontal) - 1):
+            for y in range(1, len(self.rho_vertical) - 1):
+                puzzle.insert(self.cut_image(x, y, 3), x, y, 3)
         return puzzle
 
-    def cut_image(self, x, y):
+    def cut_image(self, x, y, mode=0):
         """ Cuts part of an image depending on position of lines.
         :param x: position
         :param y: position
+        :param mode: mode=0 - cuts normally, mode=1 cuts shifted in x, mode=2 cuts shifted in y, mode=3 cuts shifted
+        in x and y
         :return: part of an image
         """
-        return self.img_gray[2 + self.rho_horizontal[x]: self.rho_horizontal[x + 1],
-                             2 + self.rho_vertical[y]: self.rho_vertical[y + 1] - 1]
+        if mode == 0:
+            i1, i2 = 2 + self.rho_horizontal[x], self.rho_horizontal[x + 1] - 1
+            j1, j2 = 2 + self.rho_vertical[y], self.rho_vertical[y + 1] - 1
+        elif mode == 1:
+            assert (0 < x < len(self.rho_horizontal) - 1)
+            i1, i2 = 2 + (self.rho_horizontal[x - 1] + self.rho_horizontal[x]) / 2.0, \
+                     (self.rho_horizontal[x] + self.rho_horizontal[x + 1]) / 2.0 - 1
+            j1, j2 = 2 + self.rho_vertical[y], self.rho_vertical[y + 1] - 1
+        elif mode == 2:
+            assert (0 < y < len(self.rho_vertical) - 1)
+            i1, i2 = 2 + self.rho_horizontal[x], self.rho_horizontal[x + 1] - 1
+            j1, j2 = 2 + (self.rho_vertical[y - 1] + self.rho_vertical[y]) / 2.0 - 1, \
+                     (self.rho_vertical[y] + self.rho_vertical[y + 1]) / 2.0 - 1
+        elif mode == 3:
+            assert(0 < x < len(self.rho_horizontal) - 1 and 0 < y < len(self.rho_vertical) - 1)
+            i1, i2 = 2 + (self.rho_horizontal[x - 1] + self.rho_horizontal[x]) / 2.0, \
+                     (self.rho_horizontal[x] + self.rho_horizontal[x + 1]) / 2.0 - 1
+            j1, j2 = 2 + (self.rho_vertical[y - 1] + self.rho_vertical[y]) / 2.0 - 1, \
+                     (self.rho_vertical[y] + self.rho_vertical[y + 1]) / 2.0 - 1
+        else:
+            i1, i2, j1, j2 = 0, 0, 0, 0
+        i1, i2, j1, j2 = int(i1), int(i2), int(j1), int(j2)
+        return self.img_rgb[i1: i2, j1: j2]
--- symapix/puzzle/container.py
+++ symapix/puzzle/container.py
@@ -5,35 +5,107 @@
 import cv2
 import numpy as np
 import pickle
+import math
 
 __author__ = 'Adriana Borowa'
 __email__ = 'ada.borowa@gmail.com'
 
 
+def dist(x, y):
+    """Return distance between points, given that point are color (1-2 = 255)"""
+    x = np.array(x, float)
+    y = np.array(y, float)
+    # res = math.sqrt(((x[0]-y[0]) % 255)**2 + ((x[1] - y[1]) % 255)**2 + ((x[2] - y[2]) % 255)**2)
+    res = math.sqrt((x[0]-y[0])**2 + (x[1] - y[1])**2 + (x[2] - y[2])**2)
+    return res
+
+
 class Container:
     """Stores puzzle data."""
+
     def __init__(self, size):
         """Initailization of container.
            Size: width and height of puzzle
         """
         self.size = size
-        # TODO: create empty array with position for squares and lines
+        self.puzzle = np.zeros((self.size[0] * 2 - 1, self.size[1] * 2 - 1))
+        self.colors = []
+        self.clf_dir = '../../classificators/'
+        self.sq_clf = pickle.load(open(self.clf_dir + 'sqclf.p', 'rb'))
+        self.horiz_clf = pickle.load(open(self.clf_dir + 'horizclf.p', 'rb'))
+        self.vert_clf = pickle.load(open(self.clf_dir + 'vertclf.p', 'rb'))
+        self.x_clf = pickle.load(open(self.clf_dir + 'xclf.p', 'rb'))
         pass
 
-    def insert(self, image, x, y):
+    def insert(self, img_rgb, x, y, mode):
         """
         Inserts data into puzzle.
-        :param image: fragment of full image to be processed
+        :param img_gray: fragment of full image to be processed in gray
+        :param img_rgb: fragment of full image to be processed in rgb
         :param x: position
         :param y: position
+        :param mode: part of puzzle, 0 - window, 1 - horizontal line, 2 - vertical line, 3 - line crossing
         :return: None
         """
-        # TODO: inserting into array: different for square and line (2 functions)
-        pass
+        img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+        image = cv2.resize(img_gray, (20, 20))
+        number = 0
+        i, j = 0, 0
+        if mode == 0:
+            number = self.sq_clf.predict(image.reshape(1, -1))
+            i = 2 * x
+            j = 2 * y
+        elif mode == 1:
+            number = self.horiz_clf.predict(image.reshape(1, -1))
+            i = 2 * x - 1
+            j = 2 * y
+        elif mode == 2:
+            number = self.vert_clf.predict(image.reshape(1, -1))
+            i = 2 * x
+            j = 2 * y - 1
+        elif mode == 3:
+            number = self.x_clf.predict(image.reshape(1, -1))
+            i = 2 * x - 1
+            j = 2 * y - 1
+        if number > 0:
+            number = self.get_color(img_rgb)
+        self.puzzle[i, j] = int(number)
 
     def print_puzzle(self):
         """
         Prints current puzzle.
         :return: None
         """
-        pass
+        for i, row in enumerate(self.puzzle):
+            txt = ''
+            for j, el in enumerate(row):
+                if el == 0:
+                    if i % 2 == 0 and j % 2 == 0:
+                        txt += '  '
+                    elif i % 2 == 0 and j % 2 > 0:
+                        txt += '| '
+                    elif i % 2 > 0 and j % 2 == 0:
+                        txt += '_ '
+                    elif i % 2 > 0 and j % 2 > 0:
+                        txt += '+ '
+                else:
+                    txt += str(int(el)) + ' '
+            print(txt)
+
+    def get_color(self, img):
+        w, h, _ = img.shape
+        color = img[int(w/2.0), int(h/2.0)]
+        curr = 0
+        if len(self.colors) == 0:
+            self.colors.append(color)
+            return 1
+        for i, c in enumerate(self.colors):
+            if dist(c, color) < 10:
+                curr = i + 1
+                break
+        if curr == 0:
+            self.colors.append(color)
+            return len(self.colors)
+        else:
+            return curr
