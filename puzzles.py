94dfedb938f9a888de4deab57e5a12b6d063f5a8
ada-borowa <ada.borowa@gmail.com>
2017-01-08 01:08:56 +0100

    Main file for running program.

---
+++ puzzles.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Project created for "Programming in Python" Class.
+Sym-a-pix and fill-a-pix puzzle solver and generator.
+"""
+
+from gui.main_window import main_window
+
+__author__ = 'Adriana Borowa'
+__email__ = 'ada.borowa@gmail.com'
+
+if __name__ == '__main__':
+    main_window()
+
